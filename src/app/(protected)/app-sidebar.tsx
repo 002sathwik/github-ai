44d3b97bebe077f2cb3c78430197f098cfa59dd2
Sathwik <hsathwik002@gmail.com>
2024-12-30 17:43:44 +0530

    [feath]:Appsidebar

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -2,85 +2,161 @@
 
 "use client";
 
-import { Bot, CreditCard, LayoutDashboard, Presentation } from 'lucide-react';
+import { Bot, CreditCard, LayoutDashboard, Plus, Presentation, RibbonIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
-import Link from 'next/link'; 
+import Link from 'next/link';
 import React from 'react';
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    useSidebar,
 } from '~/components/ui/sidebar';
 import { cn } from '~/lib/utils';
+import { Button } from '~/components/ui/button';
 
-// Sidebar menu items
+
+
 const items = [
-  {
-    name: 'Dashboard',
-    icon: LayoutDashboard,
-    href: '/dashboard',
-  },
-  {
-    name: 'Q&A',
-    icon: Bot,
-    href: '/qa',
-  },
-  {
-    name: 'Meetings',
-    icon: Presentation,
-    href: '/meetings',
-  },
-  {
-    name: 'Billing',
-    icon: CreditCard,
-    href: '/billing',
-  },
+    {
+        name: 'Dashboard',
+        icon: LayoutDashboard,
+        href: '/dashboard',
+    },
+    {
+        name: 'Q&A',
+        icon: Bot,
+        href: '/qa',
+    },
+    {
+        name: 'Meetings',
+        icon: Presentation,
+        href: '/meetings',
+    },
+    {
+        name: 'Billing',
+        icon: CreditCard,
+        href: '/billing',
+    },
 ];
 
+const projects = [
+    {
+        name: 'Project 1',
+
+    },
+    {
+        name: 'Project 2',
+
+    },
+    {
+        name: 'Project 3',
+
+    },
+    {
+        name: 'Project 4',
+
+    },
+]
+
 const AppSidebar = () => {
-  const pathname = usePathname();
+    const pathname = usePathname();
+    const { open } = useSidebar();
 
-  return (
-    <Sidebar collapsible="icon" variant="floating"  >
+    return (
+        <Sidebar collapsible="icon" variant="floating"  >
 
-      <SidebarHeader className=' text-2xl font-extrabold text font-grotesk text-center text-white bg-neutral-900 border border-black'>Github-AI</SidebarHeader>
+            <SidebarHeader>
+                <div className='flex items-center gap-2 p-2 justify-center text-center bg-neutral-900 text-white'>
+                    <RibbonIcon size={30} />
+                    {open && (
+
+                        <h1 className='text-2xl font-grotesk'>GithubAI</h1>
+                    )}
+
+                </div>
+            </SidebarHeader>
 
 
-      <SidebarContent className=''>
-        <SidebarGroup>
-          <SidebarGroupLabel className='text-md font-grotesk text-gray-600'>Application</SidebarGroupLabel>
-          <SidebarGroupContent className='p-3'>
-            <SidebarMenu className='font-sora'>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.name} >
-                  <SidebarMenuButton asChild className='rounded-md'>
-                    <Link
-                      href={item.href}
-                      className={cn(
-                        'flex items-center gap-2 p-2 rounded-md font-sora',
-                        {
-                          '!bg-primary !text-white rounded-md': pathname === item.href, 
-                        }
-                      )}
-                    >
-                      {React.createElement(item.icon, { className: 'h-5 w-5' })} 
-                      <span>{item.name}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  );
+            <SidebarContent className=''>
+                <SidebarGroup>
+                    <SidebarGroupLabel className='text-md font-grotesk text-gray-600'>Application</SidebarGroupLabel>
+                    <SidebarGroupContent >
+                        <SidebarMenu className='font-sora'>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.name} >
+                                    <SidebarMenuButton asChild className='rounded-md'>
+                                        <Link
+                                            href={item.href}
+                                            className={cn(
+                                                'flex items-center gap-2 p-2 rounded-md font-sora',
+                                                {
+                                                    '!bg-primary !text-white rounded-md': pathname === item.href,
+                                                }
+                                            )}
+                                        >
+                                            {React.createElement(item.icon, { className: 'h-5 w-5' })}
+                                            <span>{item.name}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                <SidebarGroup>
+                    <SidebarGroupLabel className='text-md font-grotesk text-gray-600'>Your Projects</SidebarGroupLabel>
+                    <SidebarGroupContent >
+                        <SidebarMenu >
+                            {projects.map((item) => (
+                                <SidebarMenuItem key={item.name} >
+                                    <SidebarMenuButton asChild >
+                                        <div>
+                                            <div
+
+                                                className={cn(
+                                                    ' rounded-sm border size-6  flex items-center gap-2 p-2 r font-sora',
+                                                    {
+                                                        'bg-primary text-white': true
+                                                    }
+
+                                                )}
+                                            >
+                                                {item.name[0]}
+
+                                            </div>
+                                            <div className='text-sm font-sora'>{item.name}</div>
+                                        </div>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                            <div className="h-2 mt-3">
+                                <SidebarMenuItem>
+                                    <Link href='/create'>
+                                        {open ? (<Button variant={'outline'} className='font-sora w-fit' >
+                                            <Plus />
+                                            Create Project
+                                        </Button>) : (<div  className=' border font-sora w-fit p-1 shadow' >
+                                            <Plus />
+                                        </div>)}
+
+                                    </Link>
+                                </SidebarMenuItem>
+
+                            </div>
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+
+            </SidebarContent>
+        </Sidebar>
+    );
 };
 
 export default AppSidebar;
