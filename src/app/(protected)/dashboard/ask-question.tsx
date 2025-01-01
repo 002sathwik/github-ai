d07e96dbd5fbc6a23220a9a6568f71cdb09c37e7
Sathwik <hsathwik002@gmail.com>
2025-01-01 17:10:00 +0530

    [update]:ask-question intregratedbackend

--- src/app/(protected)/dashboard/ask-question.tsx
+++ src/app/(protected)/dashboard/ask-question.tsx
@@ -7,15 +7,30 @@ import { Textarea } from '~/components/ui/textarea';
 import UseProject from '~/hooks/use-project';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '~/components/ui/dialog';
 import { RibbonIcon } from 'lucide-react';
+import { askQuestion } from './actions';
+import { readStreamableValue } from 'ai/rsc';
 
 const AskQuestions = () => {
     const [question, setQuestion] = React.useState('');
     const [open, setOpen] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
     const { project } = UseProject();
-
-    const onSubmit = (e: React.FormEvent) => {
+    const [filesReferences, setFilesReferences] = React.useState<{ fileName: string; sourceCode: string; summary: string }[]>([]);
+    const [answers, setAnswers] = React.useState('')
+    const onSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
+        if (!project?.id) return;
+        setLoading(true);
         setOpen(true);
+        const { output, filesReferences } = await askQuestion({ question, projectId: project.id });
+        setFilesReferences(filesReferences);
+
+        for await (const delta of readStreamableValue(output)) {
+            if (delta) {
+                setAnswers(ans => ans + delta)
+            }
+        }
+        setLoading(false);
     }
 
     return (
@@ -40,7 +55,7 @@ const AskQuestions = () => {
                 </CardContent>
             </Card>
 
-            
+
             {open && (
                 <Dialog open={open} onOpenChange={setOpen}>
                     <DialogContent>
@@ -49,8 +64,16 @@ const AskQuestions = () => {
                                 <RibbonIcon size={30} />
                             </DialogTitle>
                         </DialogHeader>
-                        <p className='font-grotesk'>Your Question has been submitted to the team</p>
+                        <p className='font-grotesk'>Your Answer</p>
                         <div className="h-4"></div>
+                        <div>
+                            {answers}
+                        </div>
+                        {
+                            filesReferences.map(file => {
+                                return <span>{file.fileName}</span>
+                            })
+                        }
                         <Button className='rounded-lg font-grotesk' onClick={() => setOpen(false)}>
                             Close
                         </Button>
