f64606500ac6a92ffc5b6d1dbbd211db86123370
Sathwik <hsathwik002@gmail.com>
2025-01-01 17:09:35 +0530

    [fix]:action.ts file

--- src/app/(protected)/dashboard/actions.ts
+++ src/app/(protected)/dashboard/actions.ts
@@ -1,4 +1,4 @@
-'use client'
+'use server';
 
 import { streamText } from 'ai'
 import { createStreamableValue } from 'ai/rsc'
@@ -25,13 +25,14 @@ export async function askQuestion({ question, projectId }: Props) {
     const result = await db.$queryRaw`
     SELECT "fileName", "sourceCode", "summary",
            1 - ("summaryEmbeddings" <=> ${vectorQuery}::vector) AS "similarity"
-    FROM "SourceCodeEmbeddings"
+    FROM "SourceCodeEmbeaddings"
     WHERE 1 - ("summaryEmbeddings" <=> ${vectorQuery}::vector) > 0.5
       AND "projectId" = ${projectId}
     ORDER BY "similarity" DESC
     LIMIT 10
   ` as { fileName: string, sourceCode: string, summary: string }[]
-
+    
+    
     let context = ''
 
     for (const doc of result) {
@@ -50,7 +51,7 @@ export async function askQuestion({ question, projectId }: Props) {
       AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
       AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
       If the question is asking about code or a specific file, AI will provide the detailed answer, giving step by step instructions, including code snippets.
-    
+
      START CONTEXT BLOCK
 
         ${context}
@@ -60,9 +61,9 @@ export async function askQuestion({ question, projectId }: Props) {
     START QUESTION 
 
         ${question}
-    
+
     END QUESTION
-       
+
       AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
       If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
       AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
@@ -77,7 +78,7 @@ export async function askQuestion({ question, projectId }: Props) {
     })()
 
     return {
-        output : stream,
+        output : stream.value,
         filesReferences : result
 
     }
